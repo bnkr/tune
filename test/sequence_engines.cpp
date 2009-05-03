39ec51956d3bc68d57f2d2ab14b6adb6c38bf567
James Webber <bunkerprivate@googlemail.com>
2009-05-03 13:39:03 +0100

    Test parametric sequence engine.

---
+++ test/sequence_engines.cpp
@@ -0,0 +1,38 @@
+/*!
+\file
+\brief Test of the private implementation of the note_sequence.
+*/
+
+#include "../src/note_sequence.hpp"
+#include "../src/notes.hpp"
+
+#include <cstdlib>
+#include <cassert>
+
+int main() {
+  // Simple 0..12 step 1.
+  {
+    double concert_pitch = 440.0;
+    int start = 0;
+    int stop =  12;
+    int step = 1;
+    detail::generated_sequence gs(concert_pitch, start, stop, step);
+    unsigned int count = 0;
+    while (! gs.done()) {
+      double freq = gs.next_frequency();
+      double expected = offset_to_frequency(concert_pitch, count);
+      assert(freq == expected);
+      ++count;
+    }
+    // 13 notes from 0..12.
+    assert(count == 13);
+  }
+
+  {
+    // detail::listed_sequence ls();
+    // TODO: test this.
+  }
+
+
+  return EXIT_SUCCESS;
+}
