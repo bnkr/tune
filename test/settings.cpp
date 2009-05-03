4f9edcc81a523c3cc53a7ea3f7d4c000d80caea0
James Webber <bunkerprivate@googlemail.com>
2009-05-03 22:52:20 +0100

    Test invalid commandline syntax.

--- test/settings.cpp
+++ test/settings.cpp
@@ -7,9 +7,45 @@
 
 #include "../src/settings.hpp"
 
+#include  <boost/program_options.hpp>
+
 #include <cstdlib>
 #include <cassert>
 
 int main() {
+  // unknown opion
+  {
+    const char *argv[] = {
+      "prog",
+      "--blah"
+    };
+
+    bool reached = false;
+    try {
+      settings s(2, (char **) argv);
+      reached = true;
+    }
+    catch (boost::program_options::unknown_option &) {
+    }
+
+    assert(! reached);
+  }
+
+  // option missing argument
+  {
+    const char *argv[] = {
+      "prog",
+      "--dump"
+    };
+
+    bool reached = false;
+    try { settings s(2, (char**)argv); reached = true; }
+    catch (boost::program_options::invalid_command_line_syntax &) { }
+    assert(! reached);
+  }
+
+  // TODO:
+  //   Test note conversions (first need to implement them in settings).
+
   return EXIT_SUCCESS;
 }
